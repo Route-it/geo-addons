5e167e2e1622f704c84ba6adc4435804ea67f787
unknown <sebaseifert@gmail.com>
2015-10-30 11:39:07 -0300

    agregado de vistas y supervisor

--- geo_certifications/models.py
+++ geo_certifications/models.py
@@ -2,20 +2,20 @@
 
 from openerp import models, fields, api
 
-# class geo_certifications(models.Model):
-#     _name = 'geo_certifications.geo_certifications'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
-
 class GeoCertification(models.Model):
-	_name = 'geo.certification'
+	_name = 'certifications.certification'
 	
-	name = fields.Char()
-	value = fields.Char()
+	parte = fields.Char()
+	pozo = fields.Char()
+	operadora = fields.Many2one('res.partner',domain = [('is_company','=','True')])
+	yacimiento = fields.Selection([('chubut','Chubut'),('santa cruz','Santa Cruz')])
+	supervisor = fields.Many2one('certifications.supervisor')
+	
+class GeoCertificationSupervisor(models.Model):
+	_name = "certifications.supervisor"
+	
+	nombre = fields.Char()
+	apellido = fields.Char()
+	operacionesHechas = fields.Integer()
+	nivelOperacion = fields.Selection([('1','1'),('2','2'),('3','3')])
+	
