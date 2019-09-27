22d441f4ce91c29d8b294cce2e6ef19c83d6d3da
Diego Richi <diego.richi@gmail.com>
2019-09-26 21:22:47 -0300

    Parche 2

---
+++ geo_certifications/SQL/patch_02.sql
@@ -0,0 +1,13 @@
+select * from certifications_certification_ceyf ce inner join certification_invoice inv on ce.invoice_id = inv.id
+where ce.state in ('carga','proceso_facturacion')
+and ce.antique_register is not null
+and inv.invoice_date is not null
+--and operadora_id = 10
+
+
+update certifications_certification_ceyf mio set state = 'facturacion'
+from certifications_certification_ceyf ce inner join certification_invoice inv on ce.invoice_id = inv.id
+where mio.state in ('carga','proceso_facturacion')
+and mio.antique_register is not null
+and inv.invoice_date is not null
+and mio.id=ce.id
