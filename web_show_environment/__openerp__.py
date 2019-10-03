564b9b1a228e5e9b14231a5189f90761e4a4faeb
Diego Richi <diego.richi@gmail.com>
2019-10-02 22:52:42 -0300

    Agregado de mensaje para indicar el ambiente de testing.

---
+++ web_show_environment/__openerp__.py
@@ -0,0 +1,17 @@
+
+{
+    'name': 'Web Show Environment',
+    'version': '1.0',
+    'sequence': 3,
+    'summary': 'Show environment at top',
+    'website': 'http://www.routeit.com.ar/',
+    'description': 'Show Environment',
+    'author': 'Route IT',
+    'depends': [],
+    'data': [
+        'views/webclient_templates.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
